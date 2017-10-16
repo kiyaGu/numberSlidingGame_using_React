03d61a755d52ec93b17cb86e8089b7e5a9a2b1a7
KiyaG <hamilee.kiya@gmail.com>
2017-10-16 21:34:46 +0100

    added a test for rendering Puzzle component

---
+++ src/Puzzle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Tile = () => <div />;
+const Puzzle = () => (
+  <div>
+    <Tile />
+    <Tile />
+    <Tile />
+    <Tile />
+  </div>
+);
+
+export default Puzzle;
