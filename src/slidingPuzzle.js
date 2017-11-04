2e53a0da4140e10e920158ffb709bd8aaed940fb
KiyaG <hamilee.kiya@gmail.com>
2017-11-04 19:30:58 +0000

    included a move command to the slidingPuzzle class

--- src/slidingPuzzle.js
+++ src/slidingPuzzle.js
@@ -2,5 +2,7 @@ export class SlidingPuzzle {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.tiles = null;
   }
+  move(position) {}
 }
--- src/slidingPuzzle.test.js
+++ src/slidingPuzzle.test.js
@@ -4,7 +4,20 @@ describe("slidingPuzzle", () => {
     expect(new SlidingPuzzle()).toBeInstanceOf(SlidingPuzzle);
   });
   it("should have a width and height property", () => {
-    expect(new SlidingPuzzle(4, 5).width).toBe(4);
-    expect(new SlidingPuzzle(4, 5).height).toBe(5);
+    expect(new SlidingPuzzle(4, 5)).toHaveProperty("width");
+    expect(new SlidingPuzzle(4, 5)).toHaveProperty("height");
+  });
+
+  it("should have a move method", () => {
+    const slidingPuzzle = new SlidingPuzzle(3, 3);
+    const spyMove = jest.spyOn(slidingPuzzle, "move");
+    slidingPuzzle.move();
+    expect(spyMove).toHaveBeenCalled();
+  });
+  it("move command should be called with single argument, position number", () => {
+    const slidingPuzzle = new SlidingPuzzle(3, 3);
+    const spyMove = jest.spyOn(slidingPuzzle, "move");
+    slidingPuzzle.move(5);
+    expect(spyMove).toBeCalledWith(5);
   });
 });
